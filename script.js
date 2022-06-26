8aae388893684cbfdef991683bd3da8b1a8b7b86
Patricia <patricia-vieira2@hotmail.com>
2022-06-26 18:45:49 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,23 @@
+/* Evento de clique nos botões da página inicial */
+function clique() {
+    window.location = 'erro.html';
+}
+
+/* Evento de clique na página de erro */
+function back() {
+    window.location = 'home.html';
+}
+
+/* Eventos de clique nas redes sociais */
+
+function insta() {
+    window.open('https://www.instagram.com/', '_blank');
+}
+
+function twitter() {
+    window.open('https://twitter.com/', '_blank');
+}
+
+function face() {
+    window.open('https://www.facebook.com/', '_blank');
+}
